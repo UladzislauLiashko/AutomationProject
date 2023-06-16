1bb0feb2b1143304c197da6d52076f1056511816
Uladzislau Liashko1 <Uladzislau_Liashko1@epam.com>
2023-06-16 16:33:51 +0300

    update branch

--- src/main/java/org/Zoo/Goldfish.java
+++ src/main/java/org/Zoo/Goldfish.java
@@ -3,6 +3,6 @@ package org.Zoo;
 public class Goldfish extends Fish{
 
     public static void lookAtPeople() {
-        System.out.println("He is looking at us");
+        System.out.println("He is looking at us!");
     }
 }
